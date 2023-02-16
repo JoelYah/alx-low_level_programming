93b4acd47ea73ceed94f43cf328e542d4fd0ddd5
JoelYah <joelw9985@gmail.com>
2023-02-16 06:40:31 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+   puts("Size of a char: %i byte(s)\n", sizeof(char));
+puts("Size of a char: %i byte(s)\n", sizeof(char));
+puts("Size of a char: %i byte(s)\n", sizeof(char));
+puts("Size of a char: %i byte(s)\n", sizeof(char));
+puts("Size of a char: %i byte(s)\n", sizeof(char));
+   return (0);
+}
+
