8c473386264406075cdc385ce3ccb56508fdf307
JoelYah <joelw9985@gmail.com>
2023-02-21 06:51:15 -0800

    task 10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_to_98 - print all the numbers from input to 98
+ *@n: the atarting number
+ */
+void print_to_98(int n)
+{
+if (nv >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf()"%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
